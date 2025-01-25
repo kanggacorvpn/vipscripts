86f08a7a47a212473a845d5b025ff27282a4ffc0
kanggacorvpn <kanggacor77vpn@gmail.com>
2025-01-25 21:29:26 +0700

    Add files via upload

---
+++ ws/banner_ssh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+cat > /etc/issue.net << END
+<font color="green">gg...</font>
+<p style="text-align: center;">
+<font color="cyan"><b><big><big>KANGGACOR TUNNELING</font></b></big></big></p>
+<p style="text-align: center;">
+    <span style="color: #7FFF00;">Support us:</span>
+    <span style="color: #EFEFEF;">WA: wa.me/6287740819772</span>
+    <span style="color: #EFEFEF;">TL: t.me/kanggacorvpn</span>
+</p>
+END
